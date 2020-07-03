d9e46805d0d222b17a18baa2b277ba8a62d0f213
LaetitiaCR <webmaster.Laetitia@laposte.net>
2020-07-03 12:35:47 +0200

    compte csharp
    premier pas sur csharp

---
+++ ProjetConsoleApplicationVSCore/Compte.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ProjetConsoleApplicationVSCore
+{
+    class Compte
+    {
+
+        private int decouvertAutorise;
+        private string nom;
+        private int numero;
+        private int solde;
+        private int v1;
+        private string v2;
+        private int v3;
+        private int v4;
+
+       
+
+        public Compte()
+        {
+
+        }
+        public Compte(int _numero, string _nom, int _solde, int _decouvertAutorise)
+        {
+
+        }
+        public void Crediter()
+        {
+
+        }
+        public Boolean Debiter()
+        {
+           
+        }
+        public Boolean Superieur(Compte _autreCompte)
+        {
+
+        }
+        public string ToString()
+        {
+
+        }
+        public Boolean transferer(int _montant, Compte _compteDestinataire)
+        {
+
+        }
+    }
+}
+
---
+++ ProjetConsoleApplicationVSCore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProjetConsoleApplicationVSCore
+{
+    class Program
+    {
+       static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+            Compte c = new Compte();
+            Compte c1 = new Compte(12345, "toto", 1000, -500);
+            Console.WriteLine(c.ToString());
+
+            Console.WriteLine(c1.ToString());
+        }
+    }
+
+}
